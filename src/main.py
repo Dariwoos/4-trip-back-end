6f8f33b44e1628f1842c0c3e6ba23ddfc30f5885
Dariwoos <tst.info20@gmail.com>
2021-01-26 21:05:36 +0000

    obtener usuario

--- src/main.py
+++ src/main.py
@@ -38,23 +38,24 @@ def handle_pro():
     db.session.add(new_user) #sin este linea no se añade a la base de datos
     db.session.commit() # esta es la hermana de la que esta arriba :) 
     print(new_user.serialize())
-    return jsonify(new_user.serialize()), 200
+    return jsonify(new_user.serialize()),200
 
 @app.route('/user/pro', methods=['GET'])
 def handle_hello():
-    body = request.get_json()
-    
+    body = Userpro.query.all()
+    for x in body:
+        print(x.serialize())
+    print(body)
     return jsonify("todo bien"), 200
     
-@app.route('/viajeros', methods=['GET'])
-def get_viajeros():
-    total_viajeros = Traveler.query.all()
-
-    response_body = {
-        "msg": "estos son todos los viajeros"
-    }
-
-    return jsonify(response_body), 200
+@app.route('/pro/<int:id>',methods=['GET'])
+def get_pro(id):
+    user_pro_by_id = Userpro.query.filter_by(id=id).filter_by(is_active=False).first()
+    print(user_pro_by_id)
+    if user_pro_by_id is not None:
+        return jsonify(user_pro_by_id.serialize()), 200
+    else:
+        return jsonify("usuario no existe"),400
 
 @app.route('/viajes', methods=['GET'])
 def get_viajes():

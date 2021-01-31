b0d7f4253599d552eb35f9b24d8a822aa46760fc
whario <mariocepedaortega@gmail.com>
2021-01-31 16:32:54 +0000

    recogiendo rol de traveler

--- src/jwt_auth.py
+++ src/jwt_auth.py
@@ -1,10 +1,10 @@
 import jwt
 import datetime
 
-def generate_token(traveler, key):
-    print(traveler)
+def generate_token(traveler,rol, key):
+    print(rol)
     print(key)
-    return jwt.encode({"email":traveler, "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=60)},key)
+    return jwt.encode({"email":traveler,"rol":traveler, "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=60)},key)
 
 def decode_token(token, key):
     return jwt.decode(token, key, algorithms="HS256")
--- src/router/traveler.py
+++ src/router/traveler.py
@@ -18,14 +18,15 @@ def traveler_route(app,token_required):#esta función recibe app y token_require
     @app.route('/traveler/login', methods=['POST'])
     def login_traveler():
         body = request.get_json()
+        print(body, "este es el body")
         traveler = Traveler.query.filter_by(email=body['email']).first()
-        print(traveler)
+        print(traveler,"este es el traveler")
         if(traveler is None):
             return "el usuario no existe", 401
         is_validate = compare_pass(body['password'], encrypted_pass(traveler.password).decode("utf-8"))
         if(is_validate == False):
             return "password incorrecto", 401
 
-        token = generate_token(traveler.email, app.config['SECRET_KEY'])
+        token = generate_token(traveler.email,traveler.rol,app.config['SECRET_KEY'])
         print(token)
         return jsonify({"access_token":token}), 200

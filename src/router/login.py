5ef415f828f25fe5eeca1ffc1822cebced8e56a2
Dariwoos <tst.info20@gmail.com>
2021-02-22 09:17:30 +0000

    registerTraveler

--- src/router/login.py
+++ src/router/login.py
@@ -9,7 +9,9 @@ def login_route(app):#esta función recibe app y token_required que vienen de ma
     def login_traveler():
         body = request.get_json()
         traveler = Traveler.query.filter_by(email=body['email']).first()
-
+        
+        if traveler is None:
+            return jsonify("user not exist"),404
         if(traveler is not None):
             is_validate = compare_pass(body['password'],traveler.password)
             
@@ -18,11 +20,9 @@ def login_route(app):#esta función recibe app y token_required que vienen de ma
             token = generate_token(traveler.email,traveler.rol,traveler.id,app.config['SECRET_KEY'])#añado el rol para pasar por el token esta informacion y poder saber si tengo que ir a consultar si el usuario existe a la base de datos de traveler o de professional
             return jsonify({"access_token":token,"rol":traveler.rol}), 200
         pro = Userpro.query.filter_by(email=body["email"]).first()
-        print(pro)
-        print(pro.serialize())
-        print(body)
-        print(compare_pass(body['password'],pro.password))
-       
+
+        if pro is None :
+           return jsonify("usuario no existe"),404
         if(pro is not None):
             is_validate = compare_pass(body['password'],pro.password )
 
@@ -33,3 +33,4 @@ def login_route(app):#esta función recibe app y token_required que vienen de ma
             return jsonify({"access_token":token,"rol":pro.rol}), 200
 
         return jsonify ("user not exists"), 404
+        

8c786a2a2f4cc3812a73ed95d67e5f66af7431f9
Dariwoos <tst.info20@gmail.com>
2021-02-24 12:49:38 +0000

    quitando is none

--- src/router/login.py
+++ src/router/login.py
@@ -9,18 +9,20 @@ def login_route(app):#esta función recibe app y token_required que vienen de ma
     def login_traveler():
         body = request.get_json()
         traveler = Traveler.query.filter_by(email=body['email']).first()
-        pro = Userpro.query.filter_by(email=body["email"]).first()
-        if traveler is None and pro is None :
-            return jsonify("user not exist"),404
+        
+
         if(traveler is not None):
             is_validate = compare_pass(body['password'],traveler.password)
+            
             if(is_validate == False):
                 return "password incorrecto", 401        
             token = generate_token(traveler.email,traveler.rol,traveler.id,app.config['SECRET_KEY'])#añado el rol para pasar por el token esta informacion y poder saber si tengo que ir a consultar si el usuario existe a la base de datos de traveler o de professional
             return jsonify({"access_token":token,"rol":traveler.rol}), 200
+        pro = Userpro.query.filter_by(email=body["email"]).first()
 
         if(pro is not None):
             is_validate = compare_pass(body['password'],pro.password )
+
             if(is_validate == False):
                 return "password incorrecto", 401
 

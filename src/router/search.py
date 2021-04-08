5491a024684e748327ff7d4e7e7d9fe42336c14d
Dariwoos <tst.info20@gmail.com>
2021-04-08 19:27:10 +0000

    push search

--- src/router/search.py
+++ src/router/search.py
@@ -6,9 +6,13 @@ def search_route(app):
     @app.route('/search', methods=["POST"])
     def search():
         body=request.get_json()
-        trips = Trip.query.filter(Trip.destination.like('%'+body["search"]+'%')).all()
-        print(trips)
-        list_trips =[]
+        trips = Trip.query.filter(Trip.destination.like('%'+body["destination"]+'%')).all()
+        trip_list = []
         for trip in trips:
-            list_trips.append(trip.serialize())
-        return jsonify(list_trips)
+            trip_search = trip.serialize()
+            trip_search["needs_trip"] = trip_search["needs_trip"].split(',')
+            trip_list.append(trip_search)
+               
+        if len(trip_list)>0:
+            return jsonify(trip_list),200
+        return "not found", 404
